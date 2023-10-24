ad9a8b1caa95d30cd4a303c05810b22414bcd853
Min Thu Khine <helloworldjustin9@gmail.com>
2023-10-23 20:45:30 -0400

    Initial Commit

---
+++ Learning App/Learning_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_AppApp.swift
+//  Learning App
+//
+//  Created by Hoover on 10/23/23.
+//
+
+import SwiftUI
+
+@main
+struct Learning_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
