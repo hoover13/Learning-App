3c511589ea1a403d66913b4bfdab9dbad0c69173
Min Thu Khine <helloworldjustin9@gmail.com>
2023-10-23 20:56:44 -0400

    - rename ContentView - organize files into folders (MVVM) - add ContentModel - set ContentModell as environment object

---
+++ Learning App/ViewModels/ContentModel.swift
@@ -0,0 +1,14 @@
+//
+//  ContentModel.swift
+//  Learning App
+//
+//  Created by Hoover on 10/23/23.
+//
+
+import Foundation
+
+class ContentModel: ObservableObject {
+    
+    var text = "This is testing if this property can be used in HomeView"
+    
+}
--- Learning App/Views/HomeView.swift
+++ Learning App/Views/HomeView.swift
@@ -7,13 +7,16 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct HomeView: View {
+    
+    @EnvironmentObject var model: ContentModel
     var body: some View {
+        
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text(model.text)
         }
         .padding()
     }
@@ -21,6 +24,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        HomeView()
     }
 }
